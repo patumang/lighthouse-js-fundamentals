a5526eb540b2537ce71281871b962ac87d9cb787
Umang Patel <umangpatel1991.cmail.com>
2021-08-18 00:52:42 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -16,3 +16,26 @@ else {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age>18) {
+  console.log("You are eligible to vote.");
+}
+
+
+
+
+
+if(temprature<-40 || temprature>40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+
+
+if(!raining) {
+  console.log("Leave your umbrella at home!");
+}
